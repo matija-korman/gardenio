a5d7b5236fbe4061f550b9a92824d635eb18e860
matija-korman <matija.korman@gmail.com>
2022-07-30 12:01:12 +0200

    Init gardenio app

---
+++ src/main/java/com/gardenio/app/GardenioApplication.java
@@ -0,0 +1,13 @@
+package com.gardenio.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenioApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gardenio/app/controllers/GardenioController.java
@@ -0,0 +1,16 @@
+package com.gardenio.app.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GardenioController {
+
+	@GetMapping(value = "/configure")
+	public List<String> configure() {
+		System.out.print("Configuring garden.....");
+		return List.of("PEA","TMT","LET","BEA");
+	}
+}

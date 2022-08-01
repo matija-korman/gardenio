3d8b98cc94a9b3383e819f1499152d816db501c2
matija-korman <matija.korman@gmail.com>
2022-08-01 18:37:25 +0200

    Added home endpoint

--- src/main/java/com/gardenio/app/controllers/GardenioController.java
+++ src/main/java/com/gardenio/app/controllers/GardenioController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GardenioController {
+	
+	@GetMapping(value = "/")
+	public String home() {
+		return "Hello!";
+	}
 
 	@GetMapping(value = "/configure")
 	public List<String> configure() {
